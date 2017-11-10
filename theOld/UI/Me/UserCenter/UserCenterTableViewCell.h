af939f745e352866b99e49f9b0c298996b4d4519
拓润公司 <tuorungongsi@tuorungongsideMacmini.local>
2017-11-10 18:52:08 +0800

    用户中心

---
+++ theOld/UI/Me/UserCenter/UserCenterTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  UserCenterTableViewCell.h
+//  theOld
+//
+//  Created by 拓润公司 on 2017/11/10.
+//  Copyright © 2017年 loary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserCenterTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLab;
+
+@end
